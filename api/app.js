0fa343407e911e387364ac5fa0614628425df771
Yash <yash_g@me.iitr.ac.in>
2022-08-31 16:44:06 +0530

    build directory

--- api/app.js
+++ api/app.js
@@ -66,6 +66,10 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 //UNAUTHORIZED ROUTES
 app.use(routes);
