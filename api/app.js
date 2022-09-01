eaa92db6db44f831848ebeab4a8313392bce69ef
Yash <yash_g@me.iitr.ac.in>
2022-09-01 09:20:22 +0530

    heroku 33

--- api/app.js
+++ api/app.js
@@ -64,12 +64,14 @@ mongoose
 
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+const __dirname='../client';
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //UNAUTHORIZED ROUTES
 app.use(routes);
