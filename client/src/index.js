d53cbe2b54a8a5748fdc6420f859658642b20221
Yash <yash_g@me.iitr.ac.in>
2022-09-04 15:10:58 +0530

    similar thing

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.baseURL = 'https://thawing-ocean-39365.herokuapp.com/api';
 var Auth_Token = localStorage.getItem("token");
 // axios.defaults.headers.common['Authorization'] = Auth_Token;
 //check sentry
